c9421e46d3a89a4fa16da13344013d996f288105
Michael Donaghy <md401@srcf.ucam.org>
2020-04-21 12:44:40 +0900

    Make our interpreter actually interpret

--- src/main/java/com/github/m50d/margatroid/Margatroid.java
+++ src/main/java/com/github/m50d/margatroid/Margatroid.java
@@ -1,5 +1,6 @@
 package com.github.m50d.margatroid;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -9,9 +10,16 @@ import com.github.m50d.margatroid.ast.Literal;
 import com.github.m50d.margatroid.ast.Node;
 import com.github.m50d.margatroid.ast.Node.Catamorphism;
 import com.github.m50d.margatroid.ast.Quoted;
+import com.github.m50d.margatroid.builtins.Plus;
 import com.github.m50d.margatroid.model.Function;
+import com.github.m50d.margatroid.parser.Parser;
 
 public class Margatroid {
+	private static final Map<String, Function> DEFAULT_SCOPE = new HashMap<>();
+	static {
+		DEFAULT_SCOPE.put("+", new Plus());
+	}
+	
 	Node stage(Node ast, Map<String, Function> scope) {
 		return ast.catamorphism(new Catamorphism<Node>() {
 			@Override
@@ -31,5 +39,10 @@ public class Margatroid {
 				return new Literal(value);
 			}
 		});
+	}
+	
+	public Node run(String input) {
+		Node ast = new Parser().parse(input).collect(Collectors.toList()).get(0);
+		return stage(ast, DEFAULT_SCOPE);
 	}
 }
--- src/test/java/com/github/m50d/margatroid/MargatroidTest.java
+++ src/test/java/com/github/m50d/margatroid/MargatroidTest.java
@@ -1,12 +1,18 @@
 package com.github.m50d.margatroid;
 
+import static org.junit.Assert.*;
 import org.junit.Test;
 
-class MargatroidTest {
+import com.github.m50d.margatroid.ast.Literal;
+import com.github.m50d.margatroid.ast.Node;
+
+public class MargatroidTest {
+	Margatroid interpreter = new Margatroid();
 
 	@Test
 	public void interpreter() {
-		
+		Node result = interpreter.run("[ + 1 2 3 ]");
+		assertEquals(new Literal("6"), result);
 	}
 
 }
